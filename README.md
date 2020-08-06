92ae892dd1cd5c5ef0eb917cd69e57803827a39f
Federico Dondi <federico.dondi2@studio.unibo.it>
2020-08-06 18:10:22 +0200

    build: scaffolding

---
+++ README.md
@@ -0,0 +1,6 @@
+# homeflix
+
+![GitHub](https://img.shields.io/github/license/federico-dondi/homeflix)
+![GitHub package.json version](https://img.shields.io/github/package-json/v/federico-dondi/homeflix)
+
+Self-hosted streaming service (Netflix-like), over LAN.
---
+++ src/server.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const path = require('path')
+
+const express = require('express')
+
+const compression = require('compression')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+
+dotenv.config({ path: path.join(__dirname, '../.env') })
+
+const app = express()
+const port = process.env.PORT || 8080
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.use('/movies', require('./movies/moviesAPI'))
+
+if (process.env.NODE_ENV === 'production') {
+  const level = 1
+  const stream = fs.createWriteStream(path.join(__dirname, 'server.js.log'), { flags: 'a' })
+
+  app.use(compression({ level }))
+  app.use(morgan('combined', { stream }))
+  app.use(helmet({
+    referrerPolicy: {
+      policy: 'no-referrer'
+    },
+    permittedCrossDomainPolicies: {
+      permittedPolicies: 'none'
+    }
+  }))
+}
+
+app.listen({ port }, () => { console.log('🚀 Homeflix is Ready!') })
